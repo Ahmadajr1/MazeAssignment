17cbec8bc976df8b29a30b9ab40ba050725cc852
Ahmed AlRamadhan <ahmadajr1@gmail.com>
2022-07-25 18:00:40 +0300

    Made start and end platforms frictionless

--- Assets/MazeAssignment/Scripts/PlayerMovement.cs
+++ Assets/MazeAssignment/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 0.01f;
     Rigidbody playerRB;
-
+//hello
     // Start is called before the first frame update
     void Start()
     {
